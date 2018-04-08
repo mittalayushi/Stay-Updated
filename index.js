9091a518954973d1512e02fe619a6c64a3733741
Ayushi Mittal <writetoayushi96@gmail.com>
2018-04-08 19:54:43 +0530

    More endpoints added
    /update
    /login
    /prefcategories
    /setcategorypriority

--- index.js
+++ index.js
@@ -38,10 +38,87 @@ app.get('/', function (req, res) {
 
 var category_schema = mongoose.Schema({
     name: String
-})
+});
 var Category = mongoose.model('categories', category_schema);
 
-app.route('/new').post(function(req,res){
+var user_schema = mongoose.Schema({
+    uname: String,
+    gid: String,
+    email: String,
+    country: String,
+    pref_categories:[{
+        name:String,
+        priority: Number
+    }],
+    pref_publishers:[{
+        name:String,
+        priority: Number
+    }]
+});
+
+var user = mongoose.model('users',user_schema);
+//==============================================================================================================================
+app.route('/login').post(function(req,res){
+    var user_name = req.body.user;
+    var email_id = req.body.emailid;
+    var g_id = req.body.gid;
+
+    user.findOne({uname:user_name},function(err,info){
+        if(!info)
+        {
+            var new_user = new user({uname:user_name,gid:g_id,email:email_id});
+            new_user.save(function(nerr,ninfo){
+                if(ninfo)
+                    res.json({'msg':'Saved'});
+                else
+                    res.json({'msg':'Error'});
+            });
+        }
+        else{
+            res.json({'msg':'Already Registered'});
+        }
+    })
+});
+//==============================================================================================================================
+app.route('/update').post(function(req,res){
+    var email_id = req.body.emailid;
+    var pref_category = req.body.pref_category;
+    var pref_country = req.body.country;
+    var pref_publisher = req.body.pref_publishers;
+    pref_category = JSON.parse(pref_category);
+    pref_publishers = JSON.parse(pref_publisher);
+    //var conditions = { email:email_id}
+    user.update({email: email_id},{$set:{country:pref_country,pref_categories:pref_category,pref_publishers:pref_publishers}},function(err,info){
+        if(info){
+            res.json({'msg':'updated'});
+        }
+        if(err){
+            res.json({'msg':'error updating'})
+        }
+    })
+});
+//==============================================================================================================================
+app.route('/prefcategories').post(function(req,res){
+    var email_id = req.body.emailid;
+    user.findOne({email:email_id},'pref_categories',function(err,info){
+        if(info){
+            res.json(info); 
+        }
+    })
+});
+//==============================================================================================================================
+    app.route('/setcategorypriority').post(function(req,res){
+   var email_id = req.body.emailid;
+    var category = req.body.category;
+    user.findOneAndUpdate({email:email_id,"pref_categories.name":category},{$inc : {"pref_categories.$.priority":1}},function(err,info){
+        if(info){
+            res.json({'msg':'updated'});
+        } 
+    });
+});
+
+//==============================================================================================================================
+app.route('/newcat').get(function(req,res){
     var name = req.body.name;
     var new_cat = new Category({name:name});
     new_cat.save(function(err,info) {
@@ -52,7 +129,6 @@ app.route('/new').post(function(req,res){
         if (err) {
             console.log("Error");
         }
-
 });
 });
 
